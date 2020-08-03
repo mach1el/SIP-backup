e4c3215f3b2e2867916fd78d32bca2bc5a147b6b
mach1el <atemhacking@gmail.com>
2020-08-03 11:23:02 +0700

    Update full conf

---
+++ etc/asterisk/check_backlist.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+from asterisk.agi import *
+
+bl_phone_num = ['0937375404','0881111111']
+agi = AGI()
+
+try:
+	agi.verbose("python agi started")
+	exten = agi.get_variable("phone_num")
+	agi.verbose("User to call",exten)
+	if exten in bl_phone_num:
+		agi.set_variable('BLACKLISTED',"True")
+	else:
+		agi.set_variable('BLACKLISTED',"False")
+
+except: agi.set_variable('BLACKLISTED',"False")
